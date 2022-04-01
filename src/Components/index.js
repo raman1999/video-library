a871fb70909a8b9f5d35c2bc0159de6d870d69c0
raman1999 <45732494+raman1999@users.noreply.github.com>
2022-04-01 10:54:50 -0700

    feat: added video grid  for layout

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,2 +1,4 @@
 export { Navbar } from "./Navigation/Navigation";
 export { Footer } from "./Footer/Footer";
+export { Sidebar } from "./Sidebar/Sidebar";
+export { VideoGrid } from "./VideoGrid/VideoGrid";
