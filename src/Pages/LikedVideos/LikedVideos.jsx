9dbf0ce71d7f44745d932f87147e1892d7340da0
raman1999 <45732494+raman1999@users.noreply.github.com>
2022-04-02 10:03:41 -0700

    feat: added like page

--- src/Pages/LikedVideos/LikedVideos.jsx
+++ src/Pages/LikedVideos/LikedVideos.jsx
@@ -1,3 +1,22 @@
+import { useUserContext } from "../../Context";
+import { LikedVideoCard } from "./LikedVideoCard";
+
 export function LikedVideos() {
-  return <div>Demo page</div>;
+  const { userState, userDispatch } = useUserContext();
+  return (
+    <div className="liked-videos-container">
+      <h2 className="txt-center txt-white mg-md-bottom">
+        Liked Videos ({userState.likedVideos.length})
+      </h2>
+      <section className="videos-list flex-box">
+        {userState.likedVideos.map((video) => (
+          <LikedVideoCard
+            key={video._id}
+            video={video}
+            userDispatch={userDispatch}
+          />
+        ))}
+      </section>
+    </div>
+  );
 }
