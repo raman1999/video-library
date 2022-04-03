7df0b76a19f15537c0980813472c6f0cd0540ead
raman1999 <45732494+raman1999@users.noreply.github.com>
2022-04-03 05:44:52 -0700

    feat: added watch later page

--- src/Pages/WatchLater/WatchLater.jsx
+++ src/Pages/WatchLater/WatchLater.jsx
@@ -1,3 +1,29 @@
+import { useUserContext } from "../../Context";
+import { WatchLaterCard } from "./WatchLaterCard";
+
 export function WatchLater() {
-  return <div>Demo page</div>;
+  const {
+    userState: { watchLater },
+    userDispatch,
+  } = useUserContext();
+
+  return (
+    <div className="watch-later-container pos-relative">
+      <h2 className="txt-center txt-white mg-md-bottom">
+        Saved Videos ({watchLater.length})
+      </h2>
+      {watchLater.length == 0 && (
+        <div className="empty-videos">You haven't saved any videos yet....</div>
+      )}
+      <section className="videos-list flex-box">
+        {watchLater.map((video) => (
+          <WatchLaterCard
+            key={video._id}
+            video={video}
+            userDispatch={userDispatch}
+          />
+        ))}
+      </section>
+    </div>
+  );
 }
