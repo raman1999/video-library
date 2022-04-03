de93dc60b8c176d7e3920c42baf6d19894ce59ba
raman1999 <45732494+raman1999@users.noreply.github.com>
2022-04-03 05:27:01 -0700

    feat : added empty videos text

--- src/Pages/LikedVideos/LikedVideos.jsx
+++ src/Pages/LikedVideos/LikedVideos.jsx
@@ -2,14 +2,20 @@ import { useUserContext } from "../../Context";
 import { LikedVideoCard } from "./LikedVideoCard";
 
 export function LikedVideos() {
-  const { userState, userDispatch } = useUserContext();
+  const {
+    userState: { likedVideos },
+    userDispatch,
+  } = useUserContext();
   return (
-    <div className="liked-videos-container">
+    <div className="liked-videos-container pos-relative">
       <h2 className="txt-center txt-white mg-md-bottom">
-        Liked Videos ({userState.likedVideos.length})
+        Liked Videos ({likedVideos.length})
       </h2>
+      {likedVideos.length == 0 && (
+        <div className="empty-videos">You haven't liked any videos yet....</div>
+      )}
       <section className="videos-list flex-box">
-        {userState.likedVideos.map((video) => (
+        {likedVideos.map((video) => (
           <LikedVideoCard
             key={video._id}
             video={video}
