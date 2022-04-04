4c8baacbf925ca306633780a156103f46f30290e
raman1999 <45732494+raman1999@users.noreply.github.com>
2022-04-04 08:49:45 -0700

    feat: added single video page

--- src/Pages/SingleVideo/SingleVideo.jsx
+++ src/Pages/SingleVideo/SingleVideo.jsx
@@ -1,3 +1,58 @@
+import { useParams } from "react-router-dom";
+import { useUserContext } from "../../Context";
+import "./single_video.css";
+import { updateHistory } from "../../Services";
+import ReactPlayer from "react-player";
+import { AddToPlaylistContainer } from "../../Components";
+import { useState } from "react";
+import { UseGetAxios } from "../../Hooks/UseGetAxios";
+import { LoadingSpinner } from "../../Components";
+
 export function SingleVideo() {
-  return <div>Demo page</div>;
+  const { userDispatch } = useUserContext();
+  const { videoId } = useParams();
+  const {
+    serverData: { video },
+    isLoading,
+  } = UseGetAxios(`/api/video/${videoId}`);
+  const [playlistToggle, setPlaylistToggle] = useState(false);
+  return (
+    <>
+      {isLoading ? (
+        <div className="loader-container-1">
+          <LoadingSpinner />
+        </div>
+      ) : (
+        <div className="single-video-container flex-column">
+          <div className="single-video">
+            <ReactPlayer
+              url={`https://www.youtube.com/watch?v=${videoId}`}
+              controls={true}
+              playing={true}
+              width={"60%"}
+              height={"60vh"}
+              onStart={() =>
+                updateHistory("ADD_TO_HISTORY", video, userDispatch)
+              }
+            />
+          </div>
+          <p className="heading-4 l-sp-1 txt-white txt-bold">{video.title}</p>
+          <span className="txt-gray">{video.creator}</span>
+          <div className="action-buttons-container flex-row">
+            <i className="fas fa-lg fa-thumbs-up"></i>
+            <i className="fas fa-lg fa-bookmark"></i>
+            <span className="pos-relative">
+              <i
+                className="fas fa-lg fa-folder-plus"
+                onClick={() => setPlaylistToggle((prev) => !prev)}
+              ></i>
+              {playlistToggle && (
+                <AddToPlaylistContainer videoDetails={video} />
+              )}
+            </span>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -2,9 +2,7 @@ export { Home } from "./Home/Home";
 export { Login } from "./Authentication/Login/login";
 export { Signup } from "./Authentication/Signup/signup";
 export { VideoListing } from "./VideoListing/VideoListing";
-
 export { NotFound } from "./NotFound/NotFound";
-
 export { LikedVideos } from "./LikedVideos/LikedVideos";
 export { History } from "./History/History";
 export { Playlist } from "./Playlist/Playlist";
