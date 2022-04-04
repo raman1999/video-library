79881be147eca484abd0e19a5bebac0c4028ac25
raman1999 <45732494+raman1999@users.noreply.github.com>
2022-04-04 10:55:28 -0700

    feat: added loader

--- src/Components/index.js
+++ src/Components/index.js
@@ -4,3 +4,4 @@ export { Sidebar } from "./Sidebar/Sidebar";
 export { VideoGrid } from "./VideoGrid/VideoGrid";
 export { AddToPlaylistContainer } from "./AddToPlaylistContainer/AddToPlaylistContainer";
 export { PlaylistModal } from "./PlaylistModal/PlaylistModal";
+export { LoadingSpinner } from "./Loader/LoadingSpinner";
