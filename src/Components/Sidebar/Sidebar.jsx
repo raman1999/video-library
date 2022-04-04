42d841789520f075bddad24800c9ce48ed0f6d5b
raman1999 <45732494+raman1999@users.noreply.github.com>
2022-04-04 02:08:05 -0700

    fix: change link to navlink and added active class

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,44 +1,47 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./sidebar.css";
 
 export const Sidebar = () => {
+  function getActiveStyle({ isActive }) {
+    return isActive ? "link btn active-link" : "link btn";
+  }
   return (
     <ul className="sidebar">
       <li className="list-item">
-        <Link to="/" className="link btn">
+        <NavLink to="/" className={getActiveStyle}>
           <i className=" fas fa-home"></i>
           <span className="sidebar-icon-text ">Home</span>
-        </Link>
+        </NavLink>
       </li>
       <li className="list-item">
-        <Link to="/videos/explore" className="link btn ">
+        <NavLink to="/videos/explore" className={getActiveStyle}>
           <i className=" fas fa-compass"></i>
           <span className="sidebar-icon-text">Explore</span>
-        </Link>
+        </NavLink>
       </li>
       <li className="list-item">
-        <Link to="/videos/playlists" className="link btn ">
+        <NavLink to="/videos/playlists" className={getActiveStyle}>
           <i className=" fas fa-folder-plus"></i>
           <span className="sidebar-icon-text">Playlists</span>
-        </Link>
+        </NavLink>
       </li>
       <li className="list-item">
-        <Link to="/videos/liked" className="link btn ">
+        <NavLink to="/videos/liked" className={getActiveStyle}>
           <i className=" fas fa-thumbs-up"></i>
           <span className="sidebar-icon-text">Likes</span>
-        </Link>
+        </NavLink>
       </li>
       <li className="list-item">
-        <Link to="/videos/watchlater" className="link btn ">
+        <NavLink to="/videos/watchlater" className={getActiveStyle}>
           <i className=" fas fa-bookmark"></i>
           <span className="sidebar-icon-text">Watch Later</span>
-        </Link>
+        </NavLink>
       </li>
       <li className="list-item">
-        <Link to="/videos/history" className="link btn ">
+        <NavLink to="/videos/history" className={getActiveStyle}>
           <i className=" fas fa-clock"></i>
           <span className="sidebar-icon-text">History</span>
-        </Link>
+        </NavLink>
       </li>
     </ul>
   );
