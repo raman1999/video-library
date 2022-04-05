f74ee301911c2bcc5f3eb748ee1973a52f948d14
raman1999 <45732494+raman1999@users.noreply.github.com>
2022-04-05 03:58:16 -0700

    feat: added not found page

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,5 +1,15 @@
+import "./not_found.css";
+import { Link } from "react-router-dom";
 export function NotFound() {
-    return (
-        <div>Not found page</div>
-    )
+  return (
+    <div className="page-not-found">
+      <div className="page-not-found-box txt-center shadow-white">
+        <h2>We cannot find any matches!</h2>
+        <p>The page you are looking for is not available within us.</p>
+        <Link to="/videos/explore" className="link btn bg-theme txt-white">
+          Explore &#8594;
+        </Link>
+      </div>
+    </div>
+  );
 }
